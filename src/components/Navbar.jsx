e5bf6efbd84882898c6f846ed802cbafdb0a364a
RAJ <rajsawant2722001@gmail.com>
2024-09-10 00:36:47 +0530

    Navbar improvements

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,17 +1,58 @@
+import React, { useState } from "react";
 import logo from "../assets/kevinRushLogo.png";
+import Hero from "./Hero";
+import Technologies from "./Technologies";
+import About from "./About"
+import Experience from "./Experience";
+import Projects from "./Projects"
 import { FaLinkedin } from "react-icons/fa";
 import { FaGithub } from "react-icons/fa";
 import { FaSquareXTwitter } from "react-icons/fa6";
 import { FaInstagram } from "react-icons/fa";
+import RajSawantResume from "../assets/RajSawantResume.pdf";
 
-const Navbar = () => { const handleIconClick = (url) => {
+const Navbar = () => { 
+  const [activeComponent, setActiveComponent] = useState("");
+
+  const handleComponentClick = (component) => {
+    setActiveComponent(component); // Set the active component based on button click
+  };
+  const handleIconClick = (url) => {
   window.open(url, "_blank");
 }
   return (
+    <div>
     <nav className="mb-20 flex items-centre justify-between py-6">
-      <div className="flex flex-shrink-0 items-center">
-        <h4 className="mx-2 w-10 font-semibold">RS</h4>
-      </div>
+      <div className="flex flex-shrink-0 items-center ml-20">
+        {/* <h4 className="mx-2 w-10 font-semibold">RS</h4> */}
+      <div className="flex gap-4">
+          <button 
+            onClick={() => handleComponentClick('About')}
+            className="px-6 py-3 bg-gradient-to-r from-violet-400 to-violet-600 text-black rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+            About Me
+          </button>
+          <button 
+            onClick={() => handleComponentClick('Technologies')}
+            className="px-6 py-3 bg-gradient-to-r from-violet-400 to-violet-600 text-black rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+            Technologies
+          </button>
+          <button 
+            onClick={() => handleComponentClick('Projects')}
+            className="px-6 py-3 bg-gradient-to-r from-violet-400 to-violet-600 text-black rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+            Projects
+          </button>
+          <button 
+            onClick={() => handleComponentClick('Experience')}
+            className="px-6 py-3 bg-gradient-to-r from-violet-400 to-violet-600 text-black rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+            Experience
+          </button>
+          <button 
+            onClick={() => window.open(RajSawantResume)}
+            className="px-6 py-3 bg-gradient-to-r from-violet-400 to-violet-600 text-black rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+            View Resume
+          </button>
+          </div>
+        </div>
       <div className="m-8 flex items-center justify-centre gap-4 text-2xl">
 
    
@@ -27,6 +68,19 @@ const Navbar = () => { const handleIconClick = (url) => {
         <FaSquareXTwitter />
       </div>
     </nav>
+
+<div className="mt-4">
+{activeComponent === "Hero" && <Hero />}
+{activeComponent === "Technologies" && <Technologies />}
+{activeComponent === "Projects" && <Projects />}
+{activeComponent === "Experience" && <Experience />}
+{activeComponent === "Contact" && <Contact />}
+{activeComponent === "About" && <About />}
+
+</div>
+
+</div>
+    
   );
 }
 
--- src/components/Technologies.jsx
+++ src/components/Technologies.jsx
@@ -36,8 +36,10 @@ const Technologies = () => {
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
+          
         >
           <RiReactjsLine className="text-7xl text-cyan-400" />
+          <h1 className="text-lg text-center">ReactJS</h1>
         </motion.div>
 
         <motion.div
@@ -45,8 +47,10 @@ const Technologies = () => {
           initial="initial"
           animate="animate"
           className="rounded-2xl border-4 border-neutral-800 p-4"
+          
         >
           <SiTailwindcss className="text-7xl text-blue-400" />
+          <h1 className="text-lg text-center">Tailwind</h1>
         </motion.div>
 
         <motion.div
@@ -56,12 +60,15 @@ const Technologies = () => {
           className="rounded-2xl border-4 border-neutral-800 p-4"
         >
           <SiSpringboot className="text-7xl text-green-400" />
+          <h1 className="text-lg text-center">SpringBoot</h1>
         </motion.div>
 
         <motion.div variants={iconVariant(3)}
             initial="initial"
             animate ="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
           <FaAws className="text-7xl text-orange-400" />
+           <h1 className="text-lg text-center">AWS</h1> 
+         
         </motion.div>
 
         <motion.div 
@@ -69,6 +76,7 @@ const Technologies = () => {
         initial="initial"
         animate ="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
           <GrOracle className="text-7xl text-red-400" />
+          <h1 className="text-lg text-center">Oracle</h1>
         </motion.div>
 
         <motion.div 
@@ -76,12 +84,14 @@ const Technologies = () => {
         initial="initial"
         animate ="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
           <FaBootstrap className="text-7xl text-violet-400" />
+          <h1 className="text-lg text-center">Bootstrap</h1>
         </motion.div>
 
         <motion.div variants={iconVariant(2)}
             initial="initial"
             animate ="animate" className="rounded-2xl border-4 border-neutral-800 p-4">
           <FaDocker className="text-7xl text-sky-700" />
+          <h1 className="text-lg text-center">Docker</h1>
         </motion.div>
       </motion.div>
     </div>
