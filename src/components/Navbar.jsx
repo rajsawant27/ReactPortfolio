538c0368cf8d9889f3a60bf9fa7a5653608b4545
RAJ <rajsawant2722001@gmail.com>
2024-09-18 01:26:16 +0530

    Made mobile responsive and twitter buttom

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,37 +23,37 @@ const Navbar = () => {
   return (
     <div>
     <nav className="mb-20 flex items-centre justify-between py-6">
-      <div className="flex flex-shrink-0 items-center ml-20">
+      <div className="flex flex-shrink-0 items-center ml-4 md:ml-20">
         {/* <h4 className="mx-2 w-10 font-semibold">RS</h4> */}
-      <div className="flex gap-4">
+      <div className="flex flex-col md:flex-row flex-wrap gap-4 w-full md:w-auto">
           <button 
             onClick={() => handleComponentClick('About')}
-            className="px-6 py-3 bg-gradient-to-r from-violet-400 to-violet-600 text-black rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+            className="w-full md:w-auto px-4 py-2 sm:px-6 sm:py-3 bg-gradient-to-r from-violet-400 to-violet-600 text-black rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
             About Me
           </button>
           <button 
             onClick={() => handleComponentClick('Technologies')}
-            className="px-6 py-3 bg-gradient-to-r from-violet-400 to-violet-600 text-black rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+            className="w-full md:w-auto px-4 py-2 sm:px-6 sm:py-3 bg-gradient-to-r from-violet-400 to-violet-600 text-black rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
             Technologies
           </button>
           <button 
             onClick={() => handleComponentClick('Projects')}
-            className="px-6 py-3 bg-gradient-to-r from-violet-400 to-violet-600 text-black rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+            className="w-full md:w-auto px-2 py-1 sm:px-4 sm:py-2 md:px-6 md:py-3 bg-gradient-to-r from-violet-400 to-violet-600 text-black rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
             Projects
           </button>
           <button 
             onClick={() => handleComponentClick('Experience')}
-            className="px-6 py-3 bg-gradient-to-r from-violet-400 to-violet-600 text-black rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+            className="w-full md:w-auto px-2 py-1 sm:px-4 sm:py-2 md:px-6 md:py-3 bg-gradient-to-r from-violet-400 to-violet-600 text-black rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
             Experience
           </button>
           <button 
             onClick={() => handleIconClick('https://codolio.com/profile/rajsawant2722001')}
-            className="px-6 py-3 bg-gradient-to-r from-violet-400 to-violet-600 text-black rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+            className="w-full md:w-auto px-2 py-1 sm:px-4 sm:py-2 md:px-6 md:py-3 bg-gradient-to-r from-violet-400 to-violet-600 text-black rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
             CP Profile
           </button>
           <button 
             onClick={() => window.open(RajSawantResume)}
-            className="px-6 py-3 bg-gradient-to-r from-violet-400 to-violet-600 text-black rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
+            className="w-full md:w-auto px-2 py-1 sm:px-4 sm:py-2 md:px-6 md:py-3 bg-gradient-to-r from-violet-400 to-violet-600 text-black rounded-lg shadow-md transform transition-transform duration-300 hover:scale-105 hover:shadow-lg">
             View Resume
           </button>
           </div>
@@ -70,7 +70,9 @@ const Navbar = () => {
         <FaInstagram size={30} 
         style={{ margin: '10px', cursor: 'pointer' }} 
         onClick={() => handleIconClick('https://www.instagram.com/raj_sawant_03/')}/>
-        <FaSquareXTwitter />
+        <FaSquareXTwitter size={30} 
+        style={{ margin: '10px', cursor: 'pointer' }} 
+        onClick={() => handleIconClick('https://x.com/raj2722001')}/>
       </div>
     </nav>
 
